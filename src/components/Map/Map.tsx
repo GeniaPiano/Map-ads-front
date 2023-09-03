0f19cd296a4657f941d6effa123ceb2938531394
GeniaPiano <genia.piano@hotmail.com>
2023-09-03 09:51:46 +0200

    [FE]Added Search Context

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, {useContext, useEffect} from 'react';
 import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
 import '../../utils/fix-map-icon'
 
 import './Map.css'
 import 'leaflet/dist/leaflet.css'
+import {SearchContext} from "../../contexts/SearchContext";
 
 export const Map = () => {
+
+    const {search} = useContext(SearchContext);
+    useEffect(() => {
+        console.log('req to Api', search)
+    },[])
+
     return (
         <div className="map">
+            <h1>Search for: {search}</h1>
             <MapContainer center={[52.2322986,21.0058034]} zoom={15}>
                 <TileLayer
                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, {SyntheticEvent, useContext, useState} from 'react';
 import './Header.css'
 import {Button} from "../common/Button";
+import {SearchContext} from "../../contexts/SearchContext";
 
-export const Header = () => (
+export const Header = () => {
+
+   const [inputVal, setInputVal] = useState<string>('')
+   const {setSearch} = useContext(SearchContext)
+
+  const setSearchFromLocalState = (e: SyntheticEvent) => {
+       e.preventDefault();
+       setSearch(inputVal)
+  }
+
+    return (
     <header>
         <h1>
             <strong> Mega </strong> ogłoszenia
         </h1>
         <Button text="dodaj ogłoszenie"/>
-        <div className="searchDiv">
-            <input type="text"/>
+        <form className="searchDiv" onSubmit={setSearchFromLocalState}>
+            <input type="text"
+                   value={inputVal}
+                   onChange={(e) => setInputVal(e.target.value)}/>
             <Button text="szukaj"/>
-        </div>
+        </form>
 
     </header>
-)
+)}
